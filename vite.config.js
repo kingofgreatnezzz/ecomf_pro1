b144ca5c3aae536aeb04dd24e5fc96c07a6cc8df
kingofgreatnezzz <kingofgreatnezzz@gmail.com>
2024-07-01 15:14:24 -0700

    vite.congif

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // Ensure this line is added
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
+});
